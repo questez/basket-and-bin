25316f4cb341ca2354a00295c57ae2de15f82512
ivanari <183847722+questez@users.noreply.github.com>
2025-11-21 20:57:38 +0300

    Добавлено меню в сцене
    Добавлено меню паузы в игровой сцене

---
+++ BasketAndBin/Assets/Scripts/Player.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static int PlayerScore { get; private set; }
+}
--- BasketAndBin/Assets/Scripts/UI/InGameMenu.cs
+++ BasketAndBin/Assets/Scripts/UI/InGameMenu.cs
@@ -1,7 +1,59 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class InGameMenu : MonoBehaviour
 {
+    [SerializeField] private AudioSource clickSound;
+    [SerializeField] private GameObject inGameScreen;
+    [SerializeField] private GameObject PauseScreen;
+    [SerializeField] private Button pauseButton;
+    [SerializeField] private Button resumeButton1;
+    [SerializeField] private Button resumeButton2;
+    [SerializeField] private Button quitButton;
+
+    private void Start()
+    {
+        inGameScreen.SetActive(true);
+        PauseScreen.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        pauseButton.onClick.AddListener(Pause);
+        resumeButton1.onClick.AddListener(Resume);
+        resumeButton2.onClick.AddListener(Resume);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    private void OnDisable()
+    {
+        pauseButton.onClick.RemoveListener(Pause);
+        resumeButton1.onClick.RemoveListener(Resume);
+        resumeButton2.onClick.RemoveListener(Resume);
+        quitButton.onClick.RemoveListener(Quit);
+    }
+
+    private void Pause()
+    {
+        clickSound.Play();
+        inGameScreen.SetActive(false);
+        PauseScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void Resume()
+    {
+        clickSound.Play();
+        inGameScreen.SetActive(true);
+        PauseScreen.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    private void Quit()
+    {
+        clickSound.Play();
+        SceneManager.LoadScene("MainMenu");
+    }
 
 }
